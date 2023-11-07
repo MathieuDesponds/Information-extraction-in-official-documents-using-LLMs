9189eef3b4c71ccc5399b0c5a76e63a1f0ef6058
mathieu-ketl-vm <mathieu.despnods@ketl.ch>
2023-11-07 10:51:05 +0000

    Continuing evaluating confidence -> need 1 token/tag

--- ner/evaluating_confidence.py
+++ ner/evaluating_confidence.py
@@ -1,6 +1,6 @@
-
-# for tag in [" 'PER'", " 'ORG'", " 'LOC'", " 'MISC'"]:
-#     print(llm.tokenize(tag.encode()))
+from llm.LLMModel import *
+from ner.llm_ner.prompt_techniques import *
+from ner.llm_ner.few_shots_techniques import *
 
 
 mapping_tag_tokens = {
@@ -11,10 +11,11 @@ mapping_tag_tokens = {
 }
 values_to_find = [l[0] for k, l in mapping_tag_tokens.items()]
 
-def get_logits_for_tags(sentence, llm, llama_ner):
-    prompt = llama_ner.get_prompts(sentence=sentence, tags = [], n = 0)
-    output = llm(prompt, stop = ["<end_output>"])['choices'][0]['text']
-    idx_tags = [i for i, val in enumerate(llm.eval_tokens) if val in values_to_find]
-    logits = llm.eval_logits
-    logits_for_tags = [{tag: logits[idx-1][mapping_tag_tokens[tag][0]] for tag in mapping_tag_tokens} for idx in idx_tags]
+def get_logits_for_tags(sentence : str, llm: LLMModel, pt : PromptTechnique):
+    prompt = pt.get_prompts_runnable(sentence)[0][0]
+    output = llm(prompt, stop = ["<end_output>"])#['choices'][0]['text']
+    generated_tokens = list(llm.model.eval_tokens)[-output['usage']['completion_tokens']:]
+    generated_logits = list(llm.model.eval_logits)[-output['usage']['completion_tokens']:]
+    idx_tags = [i for i, val in enumerate(generated_tokens) if val in values_to_find]
+    logits_for_tags = [{tag: generated_logits[idx][mapping_tag_tokens[tag][0]] for tag in mapping_tag_tokens} for idx in idx_tags]
     return logits_for_tags, output
