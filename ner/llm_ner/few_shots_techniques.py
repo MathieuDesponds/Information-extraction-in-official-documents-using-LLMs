d37d7a0ee21b887c1ed3c1667a2f3b28b6a935cc
Mathieu Desponds <mathieu.desponds@ketl.ch>
2023-11-10 09:16:17 +0100

    Add save of few shot choices

--- ner/llm_ner/few_shots_techniques.py
+++ ner/llm_ner/few_shots_techniques.py
@@ -4,9 +4,11 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import random
 
-from ner.utils import get_embbeding, sentence_transformer
+from ner.utils import get_embbeding, sentence_transformer, load, dump
 from ner.Datasets.MyDataset import MyDataset
 
+SAVED_FEW_SHOTS_SENTENCE = "./ner/saves/few_shots_saved/sentence.pkl"
+SAVED_FEW_SHOTS_ENTITY   = "./ner/saves/few_shots_saved/entity.pkl"
 
 class FewShotsTechnique(ABC) :
     def __init__(self, training_dataset : MyDataset, nb_few_shots = 5):
@@ -24,6 +26,10 @@ class FewShotsTechnique(ABC) :
     def __str__(self):
         pass
 
+    @abstractmethod
+    def save_few_shots(self):
+        pass
+
 
     
 class FST_NoShots(FewShotsTechnique):        
@@ -37,11 +43,7 @@ class FST_NoShots(FewShotsTechnique):
     def __str__(self):
         return "no-shots"
     
-class FST_Random(FewShotsTechnique): 
-    def __init__(self, training_dataset: MyDataset, nb_few_shots=5):
-        super().__init__(training_dataset, nb_few_shots)   
-
-        
+class FST_Random(FewShotsTechnique):        
     def get_nearest_neighbors(self, sentence : str)-> list[str]:
         random_rows = [self.training_dataset[i] 
                        for i in random.sample(range(len(self.training_dataset)), self.nb_few_shots)]
@@ -55,11 +57,18 @@ class FST_Random(FewShotsTechnique):
         return "random"
     
 class FST_Sentence(FewShotsTechnique):    
+    def __init__(self, training_dataset: MyDataset, nb_few_shots=5):
+        super().__init__(training_dataset, nb_few_shots)
+        self.few_shots_save = load(SAVED_FEW_SHOTS_SENTENCE)
+
     def get_nearest_neighbors(self, sentence : str)-> list[str]:
+        if sentence in self.few_shots_save and len(self.few_shots_save[sentence]) >= self.nb_few_shots :
+            return self.few_shots_save[sentence][:self.nb_few_shots]
         sentence_embedding = sentence_transformer.encode(sentence)
         similarities = cosine_similarity([sentence_embedding], self.training_dataset['sentence_embedding'])
         top_k_indices = np.argsort(similarities[0])[-self.nb_few_shots:][::-1]
         nearest_neighbors = [self.training_dataset[int(i)] for i in list(top_k_indices)]
+
         return nearest_neighbors
     
     @staticmethod
@@ -68,8 +77,15 @@ class FST_Sentence(FewShotsTechnique):
     
     def __str__(self):
         return "sentence"
+
+    def save_few_shots(self):
+        dump(self.few_shots_save , SAVED_FEW_SHOTS_SENTENCE)
     
 class FST_Entity(FewShotsTechnique):
+    def __init__(self, training_dataset: MyDataset, nb_few_shots=5):
+        super().__init__(training_dataset, nb_few_shots)
+        self.few_shots_save = load(SAVED_FEW_SHOTS_ENTITY)
+
     @staticmethod
     def name():
         return "entity"
@@ -97,3 +113,6 @@ class FST_Entity(FewShotsTechnique):
         bests = list(dict.fromkeys(bests))[:self.nb_few_shots]
         nearest_neighbors = [self.training_dataset.dataset[best] for best in bests]
         return nearest_neighbors
+    
+    def save_few_shots(self):
+        dump(self.few_shots_save , SAVED_FEW_SHOTS_ENTITY)
