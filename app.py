d07fdd0d4b98dadb8f601a4c1d34be5f5ba5b1e9
Mathieu Desponds <mathieu.desponds@ketl.ch>
2024-01-23 13:47:01 +0000

    Save prompts

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def get_mistral_model():
 
 app = Flask(__name__)
 
-model = get_mistral_instruct_model()
+# model = get_mistral_instruct_model()
 
 
 @app.route(BASE_PATH+'/', methods=['GET'])
@@ -68,27 +68,31 @@ def saving_prompt():
     data = request.get_json()  # Assuming JSON payload, adjust as needed
     if 'messages' not in data:
         return 'Missing "prompt" in the POST request data.'
+    
+    messages = data['messages']
     label = get_prompt_type_from_messages(messages)
+    file_name = get_filename_from_messages(messages)
+    
+    if label == 'Type de document' and file_name in doc_type_from_file_name: 
+        output = "{" + f""" "Type de document" : "{doc_type_from_file_name[file_name]}" """ + '}'
+    else : 
+        output = "{}"
+
     prompts.append({
         "messages" : messages,
-        "label" : label
+        "label" : label,
+        "file_name" : file_name,
+        "fields_name_key" : data['fields_name_key'],
+        "output" : output
     })
-    if label == 'Type de document' : 
-        start_file_name = messages_string.find("pseudo_")
-        if start_file_name == -1 :
-            start_file_name = messages_string.find("Pseudo_")
-        end_file_name = messages_string.find(".pdf")
-        file_name = messages_string[start_file_name, end_file_name+4]
+    return output
 
-        return "{" + f"'Type de document' : {doc_type_from_file_name[file_name]}" + '}'
-    return {}
-
-@app.route(BASE_PATH+'/save_prompt', methods=['GET'])
-def save_prompt():
+@app.route(BASE_PATH+'/save_prompts', methods=['POST'])
+def save_prompts():
+    global prompts
     dump(prompts, f"ketl/prompts_{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.csv""")
     output = f"{len(prompts)} prompts saved" 
     prompts = []
-
     return output
 
 
@@ -96,6 +100,15 @@ def get_prompt_type_from_messages(messages):
     messages_string = " ".join([m['content'] for m in messages])
     label = "Type de document" if "Type de document"  in messages_string else ("Client" if "from the client" in messages_string else "Other")
     return label
+
+def get_filename_from_messages(messages) :
+    messages_string = " ".join([m['content'] for m in messages])
+    start_file_name = messages_string.find("pseudo_")
+    if start_file_name == -1 :
+        start_file_name = messages_string.find("Pseudo_")
+    end_file_name = messages_string.find(".pdf")
+    file_name = messages_string[start_file_name: end_file_name+4]
+    return file_name
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=45505)
